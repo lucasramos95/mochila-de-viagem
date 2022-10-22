8d5ec22ee0aa35feaeab4dd18721b09a6c83d356
Lucas <lucas_ramos95@outlook.com>
2022-10-22 00:09:40 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const form = document.getElementById("novoItem")
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault()
+
+    criaElemento(evento.target.elements['nome'].value, evento.target.elements['quantidade'].value)
+})
+
+function criaElemento(nome, quantidade) {
+    const novoItem = document.createElement("li")
+    novoItem.classList.add("item")
+
+    const numeroItem = document.createElement("strong")
+    numeroItem.innerHTML = quantidade
+}
