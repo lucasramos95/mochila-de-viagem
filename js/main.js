addf4205d7f3db3e645a1f007cf824b4ce48ae93
Lucas <lucas_ramos95@outlook.com>
2022-10-27 01:26:45 -0300

    commit #07 - Código refatorado + função para atualizar elemento HTML

--- js/main.js
+++ js/main.js
@@ -12,9 +12,25 @@ form.addEventListener("submit", (evento) => {
     const nome = evento.target.elements['nome']
     const quantidade = evento.target.elements['quantidade']
 
+    const existe = itens.find( elemento => elemento.nome === nome.value )
+
     const itemAtual = {
         "nome": nome.value,
         "quantidade": quantidade.value
+    }
+
+    if (existe) {
+        itemAtual.id = existe.id
+
+        atualizaElemento(itemAtual)
+
+        itens.push(itemAtual)
+    } else {
+        itemAtual.id = itens.length
+
+        criaElemento(itemAtual)
+
+        itens.push(itemAtual)
     }
 
     criaElemento(itemAtual)
@@ -33,9 +49,14 @@ function criaElemento(item) {
 
     const numeroItem = document.createElement("strong")
     numeroItem.innerHTML = item.quantidade
+    numeroItem.dataset.id = item.id
     novoItem.appendChild(numeroItem)
 
     novoItem.innerHTML += item.nome
 
     lista.appendChild(novoItem)
+}
+
+function atualizaElemento(item) {
+    document.querySelector("[data-id=+'"+item.id+"']").innerHTML = item.quantidade
 }
